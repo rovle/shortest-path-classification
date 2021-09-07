a71ce27c7f8b2128b6447e14bacc11e911abe621
rovle <lovre.pesut@gmail.com>
2021-09-07 11:13:48 +0200

    some more refactoring

--- algorithm/base.py
+++ algorithm/base.py
@@ -1,14 +1,16 @@
-import networkx as nx
 from networkx.algorithms.shortest_paths.weighted import single_source_dijkstra
-from statistics import median
+import networkx as nx
+
 from itertools import combinations, product
 from collections import namedtuple
+from statistics import median
 
 
 
 class ShortestPathModel():
-    def __init__(self, sim_function, ):
-        self.sim_function = sim_function
+    def __init__(self, weight_fn, ):
+        self.weight_fn = weight_fn
+        self.has_been_fit = False
 
     def fit(self, X):        
         X_enumerated = list(enumerate(X))
@@ -18,15 +20,16 @@ class ShortestPathModel():
 
         self.graph = nx.Graph()
         for x_1, x_2 in combinations(X_enumerated, 2):
-            similarity = self.sim_function(x_1.features,
+            weight = self.weight_fn(x_1.features,
                                             x_2.features)
-            if similarity is not float('inf'):
+            if weight is not float('inf'):
                 self.graph.add_edge(str(x_1.index), str(x_2.index),
-                                weight=similarity)
+                                weight=weight)
         
         self.distances = single_source_dijkstra(self.graph, '0')[0]
         self.decision_boundary = median(self.distances.values())
 
+        self.has_been_fit = True
         return self
         
     def fit_predict(self, X):
@@ -40,27 +43,25 @@ class ShortestPathModel():
         
         return self
 
-    def predict(self, X, keep_new_nodes = False):
-        if not self.distances:
+    def predict(self, X, keep_new_nodes=False):
+        if not self.has_been_fit:
             raise ReferenceError("The model has not been fitted yet.")
         num_X = len(X)
         for new_node_num, old_node in product(range(num_X), self.graph.nodes):
-            similarity = self.sim_function(X[new_node_num], old_node)
-            if similarity is float('inf'):
-                continue
-            self.graph.add_edge(f"new_node_ {new_node_num}",
-                                            old_node,
-                                            self.sim_function(X[new_node_num],
-                                                                old_node)
-                                            )
+            weight = self.weight_fn(X[new_node_num], old_node)
+            if weight is not float('inf'):
+                self.graph.add_edge(f"new_node_ {new_node_num}",
+                                    old_node, weight)                                    )
         distances_new = single_source_dijkstra(self.graph,
                                                 '0')[0][len(self.graph.nodes):]
 
         predictions = \
                     [            
-                    1 if distances_new[x] > self.decision_boundary
+                    1 if distances_new[x] < self.decision_boundary
                     else 0 for x in range(num_X)
                     ]
         
-        self.graph.remove_nodes_from([f'new_node_{num}' for num in range(num_X)])
+        if not keep_new_nodes:
+            self.graph.remove_nodes_from([f'new_node_{num}' for num in range(num_X)])
+
         return predictions
